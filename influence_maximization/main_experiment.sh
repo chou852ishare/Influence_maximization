f1d396c58bcf02e4bdadde4aecbe1acb1e55a31f
chou852ishare <chouzyz@gmail.com>
2015-08-25 05:25:30 -0400

    add SocEpinions lp

--- influence_maximization/main_experiment.sh
+++ influence_maximization/main_experiment.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 for S in $(seq 5 5 50); do
-    for T in $(seq 3 2 10); do
-        nohup python experiment_lp_mip_benders.py ${S} ${T} heplt2 benders & # lp mip
-        if [ ${S} == 5 ] && [ ${T} == 1 ]; then
-            echo "sleep 5m"
-            sleep 5m
-        fi
+    for T in $(seq 1 1 1); do
+        nohup python experiment_lp_mip_benders.py ${S} ${T} epinions lp & # benders lp mip
+#        if [ ${S} == 5 ] && [ ${T} == 1 ]; then
+#            echo "sleep 5m"
+#            sleep 5m
+#        fi
     done
 done
