e1c021ce225f550c631a86fbafeb82afb0f322db
chou852ishare <chouzyz@gmail.com>
2015-09-01 09:19:09 -0400

    add simpath epinions

--- influence_maximization/goyal_package/run.sh
+++ influence_maximization/goyal_package/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-m=greedy # ldag simpath
+m=simpath # ldag simpath
 n=epinions
 for S in $(seq 5 5 50); do
     nohup ./InfluenceModels -c config_${m}.txt -budget ${S} -outdir ./output/${n}/${n}_${S}_0_${m}/ -probGraphFile ./graph/${n}.inf &
