8f8e835c44bdcbdc707627399072215d99036436
chou852ishare <chouzyz@gmail.com>
2015-09-02 00:05:27 -0400

    add epinions goyal

--- influence_maximization/goyal_package/run.sh
+++ influence_maximization/goyal_package/run.sh
@@ -2,6 +2,6 @@
 
 m=greedy # ldag simpath
 n=epinions
-for S in $(seq 5 5 50); do
+for S in $(seq 35 5 50); do
     ./InfluenceModels -c config_${m}.txt -budget ${S} -outdir ./output/${n}/${n}_${S}_0_${m}/ -probGraphFile ./graph/${n}.inf
 done
--- influence_maximization/main_experiment.sh
+++ influence_maximization/main_experiment.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 n=epinions
-m=maxweight
-for S in $(seq 25 5 50); do
-    for T in $(seq 1 2 5); do
+m=greedy
+for S in $(seq 5 5 50); do
+    for T in $(seq 3 2 3); do
         python experiment_lp_mip_benders.py ${S} ${T} ${n} ${m}
     done
 done
