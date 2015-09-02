09dcde4f4352965f07aadd64e707d01bf6735346
chou852ishare <chouzyz@gmail.com>
2015-09-01 22:06:27 -0400

    feed benders T=3

--- influence_maximization/main_experiment.sh
+++ influence_maximization/main_experiment.sh
@@ -2,8 +2,8 @@
 
 n=epinions
 m=maxweight
-for S in $(seq 15 5 50); do
-    for T in $(seq 1 2 7); do
+for S in $(seq 25 5 50); do
+    for T in $(seq 1 2 5); do
         python experiment_lp_mip_benders.py ${S} ${T} ${n} ${m}
     done
 done
