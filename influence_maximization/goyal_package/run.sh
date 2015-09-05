bb71ea1d4df9a5fcc2c0b643f3a7556f8de7c7cb
chou852ishare <chouzyz@gmail.com>
2015-09-05 11:48:01 -0400

    finish pwh

--- influence_maximization/goyal_package/run.sh
+++ influence_maximization/goyal_package/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-m=greedy # ldag simpath
+m=simpath # ldag greedy 
 n=pwh
 for S in $(seq 5 5 50); do
     ./InfluenceModels -c config_${m}.txt -budget ${S} -outdir ./output/${n}/${n}_${S}_0_${m}/ -probGraphFile ./graph/${n}.inf
--- influence_maximization/main_experiment.sh
+++ influence_maximization/main_experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 n=pwh
-m=benders
+m=maxweight
 for S in $(seq 5 5 50); do
     for T in $(seq 1 2 3); do
         python experiment_lp_mip_benders.py ${S} ${T} ${n} ${m}
